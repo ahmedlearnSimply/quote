b872631dedc358051ed59aefb8edab5389fc1966
ahmed adel <ahmedadel123422@gmail.com>
2025-03-10 02:33:40 +0200

    commit

---
+++ lib/core/functions/dialog.dart
@@ -0,0 +1,36 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:quote/core/utils/appcolors.dart';
+import 'package:quote/core/utils/textstyle.dart';
+import 'dart:ui';
+
+showErrorDialog(BuildContext context, String text) {
+  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+    backgroundColor: AppColors.redColor,
+    content: Text(
+      text,
+      style: getSmallStyle(
+        color: Colors.white,
+      ),
+      textAlign: TextAlign.center,
+    ),
+  ));
+}
+
+showLoadingDialog(BuildContext context) {
+  showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (context) => Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // Lottie.asset(
+              //     // 'assets/images/loading.json',
+              //     width: 200,
+              //     height: 200,
+              //   ),
+            ],
+          ));
+}
---
+++ lib/core/utils/textstyle.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:quote/core/utils/appcolors.dart';
+
+//* get title
+TextStyle getTitleStyle(
+    {Color? color,
+    double? fontSize = 18,
+    FontWeight? fontWeight = FontWeight.bold}) {
+  return TextStyle(
+    fontSize: fontSize,
+    color: color ?? AppColors.primary,
+    fontWeight: fontWeight,
+    fontFamily: 'cairo',
+  );
+}
+
+//* get body style
+TextStyle getBodyStyle(
+        {Color? color,
+        double? fontSize = 18,
+        FontWeight? fontWeight = FontWeight.w400}) =>
+    TextStyle(
+      fontSize: fontSize,
+      color: color ?? AppColors.black,
+      fontWeight: fontWeight,
+      fontFamily: 'cairo',
+    );
+
+//* get small style
+TextStyle getSmallStyle(
+        {Color? color,
+        double? fontSize = 12,
+        FontWeight? fontWeight = FontWeight.w500}) =>
+    TextStyle(
+      fontSize: fontSize,
+      fontFamily: 'cairo',
+      color: color ?? AppColors.black,
+      fontWeight: fontWeight,
+    );
