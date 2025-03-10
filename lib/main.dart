770aaf83a7dc084948359030cd69dea824ed17cf
ahmed adel <ahmedadel123422@gmail.com>
2025-03-10 03:27:43 +0200

    localization

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:quote/core/utils/app_theme.dart';
 import 'package:quote/feature/onboarding/pages/onborading_page.dart';
 
 void main() {
@@ -13,7 +15,15 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: AppThemes.lightTheme,
       debugShowCheckedModeBanner: false,
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: [Locale('ar')],
+      locale: Locale('ar'),
       home: Scaffold(
         body: OnboradingPage(),
       ),
