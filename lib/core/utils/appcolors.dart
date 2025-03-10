b4b259daa7bce922c012978145ae4ba784a1e790
ahmed adel <ahmedadel123422@gmail.com>
2025-03-10 02:41:39 +0200

    commit

--- lib/core/utils/appcolors.dart
+++ lib/core/utils/appcolors.dart
@@ -2,10 +2,10 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static const Color greyColor = Color.fromARGB(255, 158, 158, 158);
-  static const Color primary = Color(0xFF0B8FAC);
-  static const Color secondary = Color(0xff7CDDC4);
-  static const Color black = Color(0xFF35364F);
-  static const Color accentColor = Color(0xFFE6EFF9);
+  static const Color primary = Color(0xFF002B5B);
+  static const Color secondary = Color(0xffFFC857);
+  static const Color soft = Color(0xFFF5F0E1);
+  static const Color accentColor = Color(0xFF8D4E58);
   static const Color white = Color(0xFFFFFFFF);
   static const Color redColor = Color(0xFFE85050);
 }
