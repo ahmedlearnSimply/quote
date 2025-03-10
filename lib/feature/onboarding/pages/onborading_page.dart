81a7c294d7f7a976b57acaf2f7ee64d03b633403
ahmed adel <ahmedadel123422@gmail.com>
2025-03-10 03:21:14 +0200

    commit

---
+++ lib/feature/onboarding/pages/onborading_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class OnboradingPage extends StatefulWidget {
+  const OnboradingPage({super.key});
+
+  @override
+  State<OnboradingPage> createState() => _OnboradingPageState();
+}
+
+class _OnboradingPageState extends State<OnboradingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return AppBar();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
+import 'package:quote/feature/onboarding/pages/onborading_page.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,11 +12,10 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+        body: OnboradingPage(),
       ),
     );
   }
