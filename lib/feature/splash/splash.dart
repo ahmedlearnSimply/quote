2d7b3de3d919ca82253e97f6760b444002cceea3
ahmed adel <ahmedadel123422@gmail.com>
2025-03-11 03:52:28 +0200

    commit

--- lib/feature/splash/splash.dart
+++ lib/feature/splash/splash.dart
@@ -1,13 +1,8 @@
-// ignore_for_file: prefer_const_literals_to_create_immutables
-
 import 'package:flutter/material.dart';
-import 'package:gap/gap.dart';
 import 'package:quote/core/functions/naviagation.dart';
 import 'package:quote/core/services/storage/local_storage.dart';
 import 'package:quote/core/utils/app_assets.dart';
 import 'package:quote/core/utils/appcolors.dart';
-import 'package:quote/core/utils/textstyle.dart';
-import 'package:quote/core/widgets/custom_button.dart';
 import 'package:quote/feature/home/presentation/home.dart';
 import 'package:quote/feature/intro/Welcome.dart';
 import 'package:quote/feature/onboarding/pages/onborading_page.dart';
@@ -20,10 +15,26 @@ class Splash extends StatefulWidget {
 }
 
 class _SplashState extends State<Splash> with SingleTickerProviderStateMixin {
-  TextEditingController name = TextEditingController();
+  late AnimationController _animationController;
+  late Animation<double> _fadeAnimation;
+  late Animation<double> _scaleAnimation;
+
   @override
   void initState() {
     super.initState();
+
+    _animationController = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 2),
+    )..forward();
+
+    _fadeAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(parent: _animationController, curve: Curves.easeIn),
+    );
+
+    _scaleAnimation = Tween<double>(begin: 0.5, end: 1.1).animate(
+      CurvedAnimation(parent: _animationController, curve: Curves.elasticOut),
+    );
 
     Future.delayed(Duration(seconds: 3), () {
       bool isOnboardingShown =
@@ -31,6 +42,7 @@ class _SplashState extends State<Splash> with SingleTickerProviderStateMixin {
               false;
       bool hasEnteredName =
           AppLocalStorage.getCachedData(key: AppLocalStorage.name) != null;
+
       if (isOnboardingShown && hasEnteredName) {
         pushReplacement(context, Home());
       } else if (isOnboardingShown) {
@@ -43,17 +55,71 @@ class _SplashState extends State<Splash> with SingleTickerProviderStateMixin {
 
   @override
   void dispose() {
+    _animationController.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: SizedBox(
-        width: 250,
-        height: 250,
-        child: Image.asset(
-          AppAssets.brain,
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [AppColors.primary, AppColors.black],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FadeTransition(
+              opacity: _fadeAnimation,
+              child: ScaleTransition(
+                scale: _scaleAnimation,
+                child: Container(
+                  decoration: BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(
+                        blurRadius: 50,
+                        color: Colors.white.withOpacity(0.3),
+                        spreadRadius: 5,
+                      ),
+                    ],
+                  ),
+                  child: Image.asset(
+                    AppAssets.brain,
+                    width: 200,
+                    height: 200,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 30),
+            FadeTransition(
+              opacity: _fadeAnimation,
+              child: Text(
+                "كن مستعدًا لتغيير حياتك! ✨",
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  shadows: [
+                    Shadow(
+                      blurRadius: 10.0,
+                      color: Colors.black.withOpacity(0.5),
+                      offset: Offset(2, 2),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(height: 40),
+            CircularProgressIndicator(
+              color: Colors.white,
+            ),
+          ],
         ),
       ),
     );
