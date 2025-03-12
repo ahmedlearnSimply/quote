69ed29d612f1258ec20a8b55284f11d24c12f97a
ahmed adel <ahmedadel123422@gmail.com>
2025-03-12 23:48:01 +0200

    commit

--- lib/feature/home/presentation/home.dart
+++ lib/feature/home/presentation/home.dart
@@ -1,5 +1,7 @@
 import 'dart:math';
 import 'package:flutter/material.dart';
+import 'package:quote/core/utils/appcolors.dart';
+import 'package:quote/core/utils/textstyle.dart';
 import 'package:quote/feature/home/model/quote.dart';
 
 class Home extends StatefulWidget {
@@ -36,69 +38,47 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('أقوال مأثورة')),
-      body: Column(
-        children: [
-          // Dropdown for selecting categories
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: DropdownButton<String>(
-              value: selectedCategory,
-              items: categories.map((String category) {
-                return DropdownMenuItem<String>(
-                  value: category,
-                  child: Text(category),
-                );
-              }).toList(),
-              onChanged: (String? newCategory) {
-                if (newCategory != null) {
-                  setState(() {
-                    selectedCategory = newCategory;
-                  });
-                  _loadQuotes(newCategory);
-                }
-              },
-            ),
-          ),
-          // PageView to show one quote per page
-          Expanded(
-            child: PageView.builder(
-              controller: _pageController,
-              itemCount: quotes.length,
-              itemBuilder: (context, index) {
-                final quote = quotes[index];
-                return Container(
-                  alignment: Alignment.center,
-                  padding: const EdgeInsets.all(20),
-                  child: Card(
-                    elevation: 4,
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10)),
-                    child: Padding(
-                      padding: const EdgeInsets.all(30),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            quote.text,
-                            style: TextStyle(
-                                fontSize: 24, fontWeight: FontWeight.bold),
-                            textAlign: TextAlign.center,
-                          ),
-                          SizedBox(height: 20),
-                          Text(
-                            '- ${quote.author}',
-                            style: TextStyle(fontSize: 18, color: Colors.grey),
-                          ),
-                        ],
+      backgroundColor: AppColors.black,
+      body: SizedBox(
+        height: double.infinity,
+        width: double.infinity,
+        child: PageView.builder(
+          scrollDirection: Axis.vertical,
+          controller: _pageController,
+          itemCount: quotes.length,
+          itemBuilder: (context, index) {
+            final quote = quotes[index];
+            return Container(
+              decoration: BoxDecoration(color: AppColors.black),
+              alignment: Alignment.center,
+              child: Card(
+                color: AppColors.black,
+                elevation: 4,
+                child: Padding(
+                  padding: const EdgeInsets.all(30),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        quote.text,
+                        style: getBodyStyle(
+                          fontSize: 30,
+                          color: Colors.white,
+                        ),
+                        textAlign: TextAlign.center,
                       ),
-                    ),
+                      SizedBox(height: 20),
+                      Text(
+                        '- ${quote.author}',
+                        style: TextStyle(fontSize: 18, color: Colors.grey),
+                      ),
+                    ],
                   ),
-                );
-              },
-            ),
-          ),
-        ],
+                ),
+              ),
+            );
+          },
+        ),
       ),
     );
   }
