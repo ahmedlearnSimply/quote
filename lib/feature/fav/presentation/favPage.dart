2dcf98bc2db4a06907a4527c023028de0555487c
ahmed adel <ahmedadel123422@gmail.com>
2025-03-13 20:33:16 +0200

    commit

--- lib/feature/fav/presentation/favPage.dart
+++ lib/feature/fav/presentation/favPage.dart
@@ -1,4 +1,8 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
+import 'package:quote/core/utils/appcolors.dart';
+import 'package:quote/feature/home/model/quote.dart';
 
 class Favpage extends StatefulWidget {
   const Favpage({super.key});
@@ -10,6 +14,30 @@ class Favpage extends StatefulWidget {
 class _FavpageState extends State<Favpage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: ListView.builder(itemBuilder: (context, index) {
+          return Center(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                height: 150,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: AppColors.greyColor,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text("asfjhdaskhf"),
+                  ],
+                ),
+              ),
+            ),
+          );
+        }),
+      ),
+    );
   }
 }
